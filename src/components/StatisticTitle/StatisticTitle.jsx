5723a9d2c77541227a54dd96e87aceee52152f78
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-01-19 16:44:42 +0200

    update

---
+++ src/components/StatisticTitle/StatisticTitle.jsx
@@ -0,0 +1,8 @@
+import css from './StatisticTitle.module.css';
+import PropTypes from 'prop-types';
+
+export const StatTitle = ({ title }) => <h2 className={css.title}>{title}</h2>;
+
+StatTitle.propTypes = {
+  title: PropTypes.string,
+};
---
+++ src/components/TransactionItems/TransactionItems.jsx
@@ -0,0 +1,16 @@
+import css from '../TransactionItems/TransactionItems.module.css';
+import PropTypes from 'prop-types';
+
+export const TransactionItems = ({ type, amount, currency }) => (
+  <tr>
+    <td>{type}</td>
+    <td>{Number(amount)}</td>
+    <td>{currency}</td>
+  </tr>
+);
+
+TransactionItems.propTypes = {
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
